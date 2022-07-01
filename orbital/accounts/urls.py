71f6fcc79e8197e1765878fe673e1c73a19fdfe2
gekhro <57522751+gekhro@users.noreply.github.com>
2022-07-01 20:36:51 +0800

    Delete orbital directory
    delete

--- orbital/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.login),
-    path('products/', views.products),
-    path('customer/', views.customer),
-    path('home/', views.home)
-]
